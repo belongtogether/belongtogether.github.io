0fdbd260ad497e0826679aded6f64fe2932ae91d
IT: Shin.Duc <shin.duc@feavn.local>
2025-02-17 14:45:38 +0700

    update style text

--- javascript.js
+++ javascript.js
@@ -1,29 +1,31 @@
+/** @format */
+
 let intervalId = null;
 function createRandomHeart() {
-    // Tạo thẻ div trái tim
-    const heart = document.createElement("div");
-    heart.className = "heart";
+  // Tạo thẻ div trái tim
+  const heart = document.createElement("div");
+  heart.className = "heart";
 
-    // Thêm chữ "haha" vào trái tim
-    const text = document.createElement("div");
-    text.className = "text";
-    text.innerText = "❤️I Miss You❤️";
-    heart.appendChild(text);
+  // Thêm chữ "haha" vào trái tim
+  const text = document.createElement("div");
+  text.className = "text";
+  text.innerText = "❤️I Miss You❤️";
+  heart.appendChild(text);
 
-    // Tính toán vị trí ngẫu nhiên
-    const x = Math.random() * (window.innerWidth - 100); // Chiều rộng
-    const y = Math.random() * (window.innerHeight - 100); // Chiều cao
+  // Tính toán vị trí ngẫu nhiên
+  const x = Math.random() * (window.innerWidth - 100); // Chiều rộng
+  const y = Math.random() * (window.innerHeight - 100); // Chiều cao
 
-    // Đặt vị trí cho thẻ trái tim
-    heart.style.left = `${x}px`;
-    heart.style.top = `${y}px`;
+  // Đặt vị trí cho thẻ trái tim
+  heart.style.left = `${x}px`;
+  heart.style.top = `${y}px`;
 
-    // Thêm trái tim vào trang
-    document.body.appendChild(heart);
-  }
+  // Thêm trái tim vào trang
+  document.body.appendChild(heart);
+}
 
-  document.getElementById("startButton").addEventListener("click", () => {
-    document.getElementById("startButton").hidden = true;
-    document.getElementById("input").readOnly = true;
-    setInterval(createRandomHeart, 500);
-  });
+document.getElementById("startButton").addEventListener("click", () => {
+  document.getElementById("startButton").hidden = true;
+  document.getElementById("input").readOnly = true;
+  setInterval(createRandomHeart, 500);
+});
